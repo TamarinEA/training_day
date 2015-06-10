236e0c16cdc02cbfc7a639b55b2df1330dc65759
TamarinEA <silfar@inbox.ru>
2015-06-10 20:03:33 +0500

    BPC-6372 only ugc created_type

--- test_kirby.sql
+++ test_kirby.sql
@@ -1,12 +1,14 @@
 ﻿WITH _products AS (
-  SELECT CASE WHEN created_at < '2015-04-01' THEN '03.01-03.14 march'
-  	      WHEN created_at < '2015-05-01' THEN '04.17-04.30 april'
-	      WHEN created_at < '2015-05-14' THEN '05.01-05.13 may1'
+  SELECT CASE WHEN created_at < '2015-03-16' THEN '03.01-03.15 march1'
+	      WHEN created_at < '2015-04-01' THEN '03.16-03.31 march2'
+  	      WHEN created_at < '2015-04-16' THEN '04.01-04.15 april1'
+  	      WHEN created_at < '2015-05-01' THEN '04.16-04.30 april2'
+	      WHEN created_at < '2015-05-15' THEN '05.01-05.14 may1'
 	      WHEN created_at < '2015-06-01' THEN '05.14-05.31 may2'
 	      ELSE 'other'
 	  END AS created_month, yml_id 
   FROM products 
-  WHERE (created_at BETWEEN '2015-04-17' AND '2015-06-01') OR (created_at BETWEEN '2015-03-01' AND '2015-03-15')
+  WHERE (created_at BETWEEN '2015-03-01' AND '2015-06-01') AND create_user_type = 'ugc'
 ),
 _hand AS (
   SELECT created_month, count(*) AS hand_made 
